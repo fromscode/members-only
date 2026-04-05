3fa315b6a698c3d3b43b08bd19ed2916b90b3583
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-04-05 12:40:41 +0000

    Create login and register endpoints

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,10 +1,49 @@
 import type { Request, Response } from "express";
+import queries from "../db/queries.js";
+import bcrypt from "bcrypt";
 
-function login(req: Request, res: Response) {
-  res.send("Hello from server");
+async function login(req: Request, res: Response) {
+  const usernameOrEmail = req.body.username as string;
+  const password = req.body.password as string;
+
+  const user = await queries.getUserByUsernameOrEmail(usernameOrEmail);
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    res.status(401).json({
+      message: "Invalid username or password",
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    token: "some token", // TODO generate token
+  });
 }
-function register(req: Request, res: Response) {
-  res.send("Hello from server");
+
+async function register(req: Request, res: Response) {
+  const [firstname, lastname, username, email, password] = req.body;
+
+  if (await queries.getUserByEmail(email)) {
+    res.status(400).json({
+      message: "Email already in use. Please login with this email instead",
+    });
+    return;
+  }
+
+  if (await queries.getUserByUsername(username)) {
+    res.status(400).json({
+      message: "Username already taken, try using a different username", // TODO : generate suggestions
+    });
+    return;
+  }
+
+  const hashedPass = await bcrypt.hash(password, 10);
+  await queries.createUser(firstname, lastname, username, email, hashedPass);
+  res.status(200).json({
+    message: "User registered succesfully",
+    token: "some token", // TODO generate token
+  });
 }
 
 const controller = {
---
+++ server/src/db/queries.ts
@@ -0,0 +1,57 @@
+import type { RowDataPacket } from "mysql2";
+import pool from "./pool.js";
+import { PassThrough } from "node:stream";
+
+export async function getUserByUsernameOrEmail(usernameOrEmail: string) {
+  let [rows] = await pool.execute<RowDataPacket[]>(
+    "SELECT * from users WHERE username = ?;",
+    [usernameOrEmail],
+  );
+
+  if (!rows.length) {
+    [rows] = await pool.execute<RowDataPacket[]>({
+      sql: "SELECT * from users WHERE email = ?;",
+      values: [usernameOrEmail],
+    });
+  }
+
+  return rows[0];
+}
+
+export async function getUserByUsername(username: string) {
+  let [rows] = await pool.execute<RowDataPacket[]>(
+    "SELECT * from users WHERE username = ?;",
+    [username],
+  );
+
+  return rows[0];
+}
+
+export async function getUserByEmail(email: string) {
+  let [rows] = await pool.execute<RowDataPacket[]>(
+    "SELECT * from users WHERE email = ?;",
+    [email],
+  );
+
+  return rows[0];
+}
+
+export async function createUser(
+  firstname: string,
+  lastname: string,
+  username: string,
+  email: string,
+  hashedPass: string,
+) {
+  await pool.query(
+    'INSERT INTO users (firstname, lastname, username, email, password, role) VALUES (?, ?, ?, ?, ?, "USER")',
+    [firstname, lastname, username, email, hashedPass],
+  );
+}
+
+export default {
+  getUserByUsernameOrEmail,
+  getUserByUsername,
+  getUserByEmail,
+  createUser,
+};
