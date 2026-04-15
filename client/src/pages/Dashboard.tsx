aa852e9149c77f28d9fe57604659bcdb1cc22f49
fromscode <chakrabortyshubham00@gmail.com>
2026-04-15 17:07:28 +0530

    Design dashboard wip

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 
 import NavBarUser from "../components/NavBarUser"
+import { Card, CardHeader, CardDescription, CardTitle, CardAction, CardContent, CardFooter } from "@/components/ui/Card"
+import { Button } from "@/components/ui/button"
+
+import { Trash2 } from "lucide-react"
 
 export default function Dashboard() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -48,9 +52,30 @@ export default function Dashboard() {
         <div>Loading</div>
       ) : (
         <>
-          {messages?.map((message) => (
-            <div key={message.id}>{message.title}</div>
-          ))}
+          <div className="flex gap-4 m-5">
+            {messages?.map((message) => (
+              <Card size="sm" className="mx-auto w-full max-w-sm bg-input p-4 gap-0!">
+                <CardHeader className="p-0!">
+                  <CardTitle className="text-xs! flex gap-3 items-center"><div className="h-5 w-5 rounded-full bg-primary"></div>{message.author}</CardTitle>
+                </CardHeader>
+                <hr className="my-2" />
+                <CardHeader className="p-0!">
+                  <CardTitle className="text-lg">{message.title}</CardTitle>
+                </CardHeader>
+                <CardDescription className="mb-4">
+                  {message.body}
+                </CardDescription>
+                <div className="flex mt-auto bg-none">
+                  <Button  className=" cursor-pointer bg-red-950 mt-auto ">
+                    Action
+                  </Button>
+                  <Button className="bg-red-950 cursor-pointer text-accent-foreground">
+                    <Trash2 />
+                  </Button>
+                </div>
+              </Card>
+            ))}
+          </div>
         </>
       )}
     </>
