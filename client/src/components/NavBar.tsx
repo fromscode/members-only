b46f1d8ccac9b20cf221c1281797c4bb2d2fcb8a
fromscode <chakrabortyshubham00@gmail.com>
2026-04-15 14:03:33 +0530

    Update navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -9,12 +9,19 @@ import {
 
 export default function NavBar() {
   return (
-    <header className="flex w-full items-center justify-between px-5 py-2">
+    <header className="flex w-full items-center justify-between px-5 py-2 gap-4">
       <NavLink
         to="/"
-        className="cursor-pointer text-xl underline underline-offset-4 hover:no-underline"
+        className="cursor-pointer text-xl py-4 px-6 bg-muted rounded-2xl hover:opacity-80"
       >
         OnlyChat
+      </NavLink>
+
+      <NavLink
+        to="/dashboard"
+        className="cursor-pointer text-xl underline underline-offset-4 hover:no-underline"
+      >
+        Dashboard
       </NavLink>
 
       <NavigationMenu>
