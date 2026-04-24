ae478951584d21097a1d0af8e874204038259511
fromscode <chakrabortyshubham00@gmail.com>
2026-04-24 17:35:13 +0530

    Update navbaruser for better ui when user is a member or admin

--- client/src/components/NavBarUser.tsx
+++ client/src/components/NavBarUser.tsx
@@ -46,10 +46,10 @@ export default function NavBaruser({ role }: { role: string }) {
 
       <NavLink to="/dashboard/upgrade" className="h-full cursor-default">
         {" "}
-        {role == "ADMIN" ? (
-          <div className="flex h-full">
-            <div className="flex h-full items-center justify-stretch gap-5 rounded-l-full border-2 border-primary bg-accent px-3 text-xs">
-              <div>{role}</div>
+        {role !== "USER" ? (
+          <div className="flex min-h-full">
+            <div className="flex min-h-full items-center justify-stretch gap-5 rounded-full border-2 border-primary bg-accent px-3 text-xs">
+              <div className="min-h-full py-2">{role}</div>
             </div>
           </div>
         ) : (
